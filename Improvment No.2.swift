a38cee7c9e4fb67baca819875db0377a1fc3d5e3
Damien-Sorrell <102154153+Damien-Sorrell@users.noreply.github.com>
2022-03-29 12:40:25 +0100

    Add files via upload
    Started working on Improvement 2 for chapter4, only filtered out pointless memory allocation stuff from C so far

---
+++ Improvment No.2.swift
@@ -0,0 +1,26 @@
+//
+//  Improvment No.2.swift
+//  Chapter 4
+//
+//  Created by (s) Damien Sorrell on 29/03/2022.
+//
+
+import Foundation
+
+
+ /* Version 2 of the improved bare-bones 1D FDTD simulation. */
+
+ 
+ func main() {
+ 
+ gridInit2(g); // Make a 2D array for a grid
+
+ezIncInit(g);
+
+for (Time = 0; Time < MaxTime; Time++) { //time stepping
+    updateH2(g)
+    updateE2(g)
+    Ez(0) = ezInc(Time, 0.0)
+    print(Ez(50))
+}
+ }
