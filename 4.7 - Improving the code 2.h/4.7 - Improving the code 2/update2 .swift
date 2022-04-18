3e82d0ca4c4d0cdab31aa634029e0e40559ad937
JackMurds <101184324+JackMurds@users.noreply.github.com>
2022-04-18 03:58:20 +0100

    Update update2 .swift

--- 4.7 - Improving the code 2.h/4.7 - Improving the code 2/update2 .swift
+++ 4.7 - Improving the code 2.h/4.7 - Improving the code 2/update2 .swift
@@ -11,12 +11,12 @@ private let imp0 = 377.0
 
 public func updateH2 (_ g: Grid) -> Void {
     for mm in 0..<g.size - 2 {
-        hz[mm] = hz[mm] + (ez[mm+1] - ez[mm]) / imp0
+        hy[mm] = hy[mm] + (ez[mm+1] - ez[mm]) / imp0
     }
 }
 
 public func updateE2 (_ g: Grid) -> Void {
     for mm in 1..<g.size - 1 {
-        ez[mm] = ez[mm] + (hz[mm] - hz[mm-1]) * imp0
+        ez[mm] = ez[mm] + (hy[mm] - hy[mm-1]) * imp0
     }
 }
