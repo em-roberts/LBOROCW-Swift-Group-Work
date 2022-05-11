7c0b33a0dabc58a0c29d369c0ef9dcd809eaee37
JackMurds <101184324+JackMurds@users.noreply.github.com>
2022-05-11 17:07:05 +0100

    Add files via upload

---
+++ Part 2 of 2D Electromagnetism - Chapter 8/2D Electromagnetism - Chapter 8/grid1dez c.swift
@@ -0,0 +1,49 @@
+//
+//  grid1dez c.swift
+//  2D Electromagnetism - Chapter 8
+//
+//  Created by (s) Jack Murdoch on 11/05/2022.
+//
+
+import Foundation
+
+public let nLoss: Int = 20
+public let maxLoss = 0.35
+
+//note - for now assumes that a predefined grid is inputted (else cdtds becomes 0)
+public func gridInit1d(_ g: Grid) -> Grid {
+    var temp = g
+    
+    let imp0: Double = 377.0
+    var depthInLayer: Double = 0.0, lossFactor: Double = 0.0
+    
+    temp.sizeX += nLoss
+    temp.type = GridType.oneDGrid.rawValue
+    
+    temp.hy = Array(repeating: 0.0, count: temp.sizeX - 1)
+    temp.chyh = Array(repeating: 0.0, count: temp.sizeX - 1)
+    temp.chye = Array(repeating: 0.0, count: temp.sizeX - 1)
+    temp.ez = Array(repeating: 0.0, count: temp.sizeX)
+    temp.cezh = Array(repeating: 0.0, count: temp.sizeX)
+    temp.ceze = Array(repeating: 0.0, count: temp.sizeX)
+    
+    for mm in 1 ..< temp.sizeX - 1 {
+        if mm < temp.sizeX - 1 - nLoss {
+            temp.ceze[mm] = 1.0
+            temp.cezh[mm] = temp.cdtds * imp0
+            temp.chyh[mm] = 1.0
+            temp.chye[mm] = temp.cdtds / imp0
+        } else {
+            depthInLayer = Double(mm - (temp.sizeX - 1 - nLoss)) + 0.5
+            lossFactor = maxLoss * pow(depthInLayer / Double(nLoss), 2)
+            temp.ceze[mm] = (1.0 - lossFactor) / (1.0 + lossFactor)
+            temp.cezh[mm] = temp.cdtds * imp0 / (1.0 + lossFactor)
+            depthInLayer += 0.5
+            lossFactor = maxLoss * pow(depthInLayer / Double(nLoss), 2)
+            temp.chyh[mm] = (1.0 - lossFactor) / (1.0 * lossFactor)
+            temp.chye[mm] = temp.cdtds / imp0 / (1.0 + lossFactor)
+        }
+    }
+    return temp
+}
+
---
+++ Part 2 of 2D Electromagnetism - Chapter 8/2D Electromagnetism - Chapter 8/main.swift
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  2D Electromagnetism - Chapter 8
+//
+//  Created by (s) Jack Murdoch on 03/05/2022.
+//
+import Foundation
+
+var t: Int
+var g = Grid(hx: [], chxh: [], chxe: [],
+             hy: [], chyh: [], chye: [],
+             hz: [], chzh: [], chze: [],
+             ex: [], cexe: [], cexh: [],
+             ey: [], ceye: [], ceyh: [],
+             ez: [], ceze: [], cezh: [],
+             sizeX: 0, sizeY: 0, sizeZ: 0,
+             time: 0, maxTime: 0,
+             type: 0,
+             cdtds: 0)
+
+g = gridInit(g)
+//abcInit()
+tfsfInit(g)
+snapshotInit()
+
+for t in 0 ..< g.maxTime {
+    g.time = t
+    updateH2d(g)
+    //tfsfUpdate(g)
+    updateE2d(g)
+    //abc(g)
+    snapshotWrite(g)
+}
+
+snapshotUpload()
+
+//unfinished
+//need to backtrack to finish tfsftmz.c (215), done 8.15 early
