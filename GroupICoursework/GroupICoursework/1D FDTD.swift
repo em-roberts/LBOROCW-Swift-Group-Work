80a5f95ce9a50a649297369692cbe40184d43509
KelseyRoe <101279279+KelseyRoe@users.noreply.github.com>
2022-05-17 20:59:14 +0100

    Integrating chapter 4
    Emily and Kelsey got up to program 4.8 inclusive with Gnuplot (unsure if the graph is correct)

--- GroupICoursework/GroupICoursework/1D FDTD.swift
+++ GroupICoursework/GroupICoursework/1D FDTD.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-public func AllocationIn1D(maxTime: Int, courantNumber: Double, time: Int) {
+public func AllocationIn1D(maxTime: Int, courantNumber: Double, timeStep: Int) {
     currentElectromagneticField.electricField = [Double] (repeating: 0.0, count: currentElectromagneticField.size)
     
     currentElectromagneticField.magneticField = [Double] (repeating: 0.0, count: currentElectromagneticField.size)
     
-    currentElectromagneticField.time = time
+    currentElectromagneticField.timeStep = timeStep
     currentElectromagneticField.maxTime = maxTime
     currentElectromagneticField.courantNumber = courantNumber
 }
@@ -20,7 +20,7 @@ public func AllocationIn1D(maxTime: Int, courantNumber: Double, time: Int) {
 public struct Grid {
     var electricField: [Double] = []
     var magneticField: [Double] = []
-    var time = 0
+    var timeStep = 0
     var maxTime = 0
     var courantNumber = 0.0
     var size = 0
