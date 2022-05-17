271a3038369efc5f3bf3ba7c7b8debf84007370d
Damien-Sorrell <102154153+Damien-Sorrell@users.noreply.github.com>
2022-05-17 12:50:28 +0100

    Compiled 4-5.1 , WIP (Damien + Liam)

---
+++ Compiled 4-5.1/Compiled/abcInit.swift
@@ -0,0 +1,17 @@
+//
+//  abcInit.swift
+//  Compiled
+//
+//  Created by (s) Damien Sorrell on 17/05/2022.
+//
+
+import Foundation
+
+public func abcInit (_ g: Grid) -> Void {
+    return //Initialiser will be used later
+}
+
+public func abc (_ g: Grid) -> Void {
+    ez[0] = ez[1]
+    return
+}
