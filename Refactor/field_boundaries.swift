40e939ac6d109bfd44c5d5a874ded5ab421775b4
em-roberts <emchloeroberts@gmail.com>
2022-05-21 02:29:12 +0100

    finishing chapter 6

--- Refactor/field_boundaries.swift
+++ Refactor/field_boundaries.swift
@@ -1,4 +1,4 @@
-private var tfsfBoundary: Int; 
+private var tfsfBoundary: Int 
 
 func tfsfInit(_ grid: Grid) -> Void {
     print("Enter the Location of the TFSF Boundary:")
@@ -12,17 +12,43 @@ func tfsfUpdate(timeStep: Int, _ grid: Grid) -> Void {
 }
 
 private var initDone: Bool = false
-private var elecFieldOldLeft = 0.0, elecFieldOldRight: Double = 0.0
-private var abcCoeffLeft = 0.0, abcCoeffRight: Double = 0.0
+
+private var elecFieldOldLeft1: [Double] = [Double](repeating: 0.0, count: 3)
+private var elecFieldOldLeft2: [Double] = [Double](repeating: 0.0, count: 3)
+private var elecFieldOldRight1: [Double] = [Double](repeating: 0.0, count: 3)
+private var elecFieldOldRight2: [Double] = [Double](repeating: 0.0, count: 3)
+
+private var abcCoeffLeft: [Double] = []
+private var abcCoeffRight: [Double] = []
 
 func abcInit(_ grid: Grid) -> Void {
     initDone = true
 
-    var temp: Double = sqrt(grid.cezh[0] * grid.chye[0])
-    abcCoeffLeft = (temp - 1.0) / (temp + 1.0)
+    // var temp: Double = sqrt(grid.cezh[0] * grid.chye[0])
+    // abcCoeffLeft = (temp - 1.0) / (temp + 1.0)
 
-    temp = sqrt(grid.cezh[grid.electric.count - 1] * grid.chye[grid.magnetic.count - 1])
-    abcCoeffRight = (temp - 1.0) / (temp + 1.0)
+    // temp = sqrt(grid.cezh[grid.electric.count - 1] * grid.chye[grid.magnetic.count - 1])
+    // abcCoeffRight = (temp - 1.0) / (temp + 1.0)
+    
+    //coeffs on left
+    var temp1: Double = sqrt(grid.cezh[0] * grid.Chye[0])
+    var temp2: Double = 1.0 / temp1 + 2.0 + temp1
+    abcCoeffLeft = [
+        -(1.0 / temp1 - 2.0 + temp1) / temp2,
+        -2.0 * (temp1 - 1.0 / temp1) / temp2,
+         4.0 * (temp1 + 1.0 / temp1) / temp2
+    ]
+
+    // coeffs on right
+    temp1 = sqrt(grid.cezh[grid.electric.count - 1] * grid.chye[grid.magnetic.count - 1])
+    temp2 = 1.0 / temp1 + 2.0 + temp1
+    abcCoeffRight = [
+        -(1.0 / temp1 - 2.0 + temp1) / temp2,
+        -2.0 * (temp1 - 1.0 / temp1) / temp2,
+         4.0 * (temp1 + 1.0 / temp1) / temp2
+    ]
+
+
 }
 
 func abc(_ grid: Grid) -> Void {
@@ -30,15 +56,27 @@ func abc(_ grid: Grid) -> Void {
         print("abcInit must be called before abc.")
         exit(-1)
     }
-    
-    // ABC for left hand side
-    grid.electric[0] = elecFieldOldLeft + abcCoeffLeft * (grid.electric[1] - grid.electric[0])
-    elecFieldOldLeft = grid.electric[1]
 
-    // ABC for right hand side
-    grid.electric[grid.electric.count - 1] = elecFieldOldRight + abcCoeffRight * (grid.electric[grid.electric.count - 2] - grid.electric[grid.electric.count - 1])
-    elecFieldOldRight = grid.electric[grid.electric.count - 2]
+    grid.electric[0] = abcCoeffLeft[0] * (grid.electric[2] + elecFieldOldLeft2[0])
+        + abcCoeffLeft[1] * (elecFieldOldLeft1[0] + elecFieldOldLeft1[2] -
+        grid.electric[1] - elecFieldOldLeft2[1])
+        + abcCoeffLeft[2] * elecFieldOldLeft1[1] - elecFieldOldLeft2[2]
 
-   // grid.magnetic[grid.magnetic.count - 1] = grid.magnetic[grid.magnetic.count - 2]
+    /* ABC for right side of grid */
+    grid.electric[grid.electric.count - 1] =
+        abcCoeffRight[0] * (grid.electric[grid.electric.count - 3] + elecFieldOldRight2[0])
+        + abcCoeffRight[1] * (elecFieldOldRight1[0] + elecFieldOldRight1[2] -
+        grid.electric[grid.electric.count - 2] - elecFieldOldRight2[1])
+        + abcCoeffRight[2] * elecFieldOldRight1[1] - elecFieldOldRight2[2]
+
+    /* update stored fields */
+    for index in 0 ..< elecFieldOldLeft1.count {
+        elecFieldOldLeft2[index] = elecFieldOldLeft1[index]
+        elecFieldOldLeft1[index] = grid.electric[index]
+        elecFieldOldRight2[index] = elecFieldOldRight1[index]
+        elecFieldOldRight1[index] = grid.electric[grid.electric.count - 1 - index]
+    }
+
 }
+
 
